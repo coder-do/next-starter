6dec827adc8080ca94febd4b11b9968af5e277c8
Meruzh Kiloyan <68124376+coder-do@users.noreply.github.com>
2021-03-12 13:55:00 +0400

    Dev (#6)
    * antd nav menu
    
    * nav

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,18 @@ import { Row, Col, Typography } from 'antd';
 import Events from '../../components/events';
 import { getFeaturedEvents } from '../../data';
 
+const { Title } = Typography;
+
 export default function events() {
     const events = getFeaturedEvents().map(item => Object.values(item));
     return (
-        <Row>
-            <Col span={24}>
-                <Events items={events} />
-            </Col>
-        </Row>
+        <>
+            <Title level={2} style={{ textAlign: 'center', marginTop: '30px' }}>Featured events</Title>
+            <Row>
+                <Col span={24}>
+                    <Events items={events} />
+                </Col>
+            </Row>
+        </>
     )
 }
