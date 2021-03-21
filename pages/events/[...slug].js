f2fbb9535f4714fdd10b5457063ba6255bc0da0c
Meruzh Kiloyan <68124376+coder-do@users.noreply.github.com>
2021-03-21 19:58:45 +0400

    Dev (#13)
    * updates
    
    * fix

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,29 @@
-import { Row, Col, Typography } from 'antd';
-
+import { Row, Col, Typography, Table } from 'antd';
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../data';
 const { Title } = Typography;
+import { columns } from '../../data/table-data';
 
 export default function eventFiltered() {
+    const router = useRouter();
+    const data = router.query.slug;
+
+    if (!data) { return <p style={{ textAlign: 'center' }}>loading...</p> }
+
+    const filteredData = getFilteredEvents({
+        year: +data[0],
+        month: +data[1]
+    });
+
     return (
         <Row>
             <Col span={12} offset={6}>
                 <Title level={2} style={{ textAlign: 'center', marginTop: '30px' }}>Event filtered page</Title>
+                <Table
+                    columns={columns}
+                    dataSource={filteredData}
+                    pagination={{ defaultPageSize: 5 }}
+                />
             </Col>
         </Row>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import { Row, Col, Typography } from 'antd';
 import Events from '../components/events';
+import EventsSearch from '../components/events/event-filter';
 import { getAllEvents } from '../data';
+import { useRouter } from 'next/router';
 
 const { Title } = Typography;
 
 const home = () => {
     const events = getAllEvents().map(item => Object.values(item));
+    const router = useRouter();
+
+    const findEvents = (year, month) => {
+        router.push(`/events/${year}/${month}`);
+    }
+
     return (
         <>
             <Title level={2} style={{ textAlign: 'center', marginTop: '30px' }}>All events</Title>
+            <EventsSearch onSearch={findEvents} />
             <Row>
                 <Col span={24}>
                     <Events items={events} />
