c6eb8843a7974a0dad5ede8366a349df46bacbe9
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-04-25 20:29:51 +0400

    multi lang add for header

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,25 +1,49 @@
-import { Menu, Row, Col, Layout } from 'antd';
+import { Menu, Row, Col, Layout, Button } from 'antd';
 import * as React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { useTranslation } from 'next-i18next';
+
 const { Header } = Layout;
 
 const Nav: React.FC = () => {
+    const router = useRouter();
     const path: string = useRouter().pathname;
+
+    const { t } = useTranslation('common');
+
     return (
         <Layout>
             <Header>
                 <Row justify="space-between" align="middle">
                     <Col flex="500px">
                         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['default']}>
-                            <Menu.Item key={path === '/' ? 'default' : ''}>
-                                <Link href='/'>Home</Link>
+                            <Menu.Item
+                                key={(path === '/' || path === '/' + router.locale) ? 'default' : ''}
+                            >
+                                <Link
+                                    href={router.locale + '/'}
+                                >{t('home')}</Link>
                             </Menu.Item>
-                            <Menu.Item key={path === '/events' ? 'default' : ''}>
-                                <Link href='/events'>Featured</Link>
+                            <Menu.Item
+                                key={(path === '/events' || path === '/events/' + router.locale) ? 'default' : ''}
+                            >
+                                <Link
+                                    href={router.locale + '/events'}
+                                >{t('featured')}</Link>
                             </Menu.Item>
                         </Menu>
+                    </Col>
+                    <Col flex="300px">
+                        <Link
+                            href={path}
+                            locale={router.locale === 'en' ? 'ge' : 'en'}
+                        >
+                            <Button>
+                                {router.locale === 'en' ? 'ge' : 'en'}
+                            </Button>
+                        </Link>
                     </Col>
                 </Row>
             </Header>
